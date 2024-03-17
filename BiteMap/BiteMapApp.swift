a7b8f639f5cacb8d3b78947937929f59f56bfdfc
kpradjinata <kpradjinata@gmail.com>
2024-03-16 22:51:13 -0700

    Initial Commit

---
+++ BiteMap/BiteMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteMapApp.swift
+//  BiteMap
+//
+//  Created by Kevin Pradjinata on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BiteMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
